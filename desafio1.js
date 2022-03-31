e112063d8f500e73bd3808c5086b751b96882a8c
Lucas Tambuscio <ltambuscio@yahoo.com>
2022-03-31 19:04:40 -0300

    Agregue js

---
+++ desafio1.js
@@ -0,0 +1,19 @@
+console.log ('Bienvenido usuario')
+
+let nombre = prompt('Ingrese nombre')
+if(nombre == '') {
+    alert('No ingresaste nombre')
+} else { 
+    alert('Bienvenido ' + nombre + '🙂') 
+} 
+
+const aa = 2022
+
+const year = prompt ('Escriba año de nacimiento')
+const edad = aa - year
+
+console.log('la edad es ' + edad)
+alert('la edad es ' + edad)
+
+ 
+ 
